b5dc3fd636588a44492e12cb09e2fa4998b2d4ff
Matbaldi <90869457+Matbaldi@users.noreply.github.com>
2021-09-17 18:02:48 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,9 @@
 module.exports = async (client) => {
-  let statuses = [
-    "++help",
-    "Music"
-  ]
+  let statuses = []
 
   setInterval(function() {
     let status = statuses[Math.floor(Math.random() * statuses.length)];
     client.user.setActivity(status, {
-      type: "LISTENING",
     });
   }, 1000);
 
